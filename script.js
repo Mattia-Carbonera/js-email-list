de92231e04623c91e1d009bcbf233a07ffd488a9
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-11-06 14:09:39 +0100

    add button genera mail

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 // ottengo i nodi
 const mailContainerEl = document.getElementById("mail-container");
+const buttonGeneraEl = document.getElementById("button-genera");
+console.log(buttonGeneraEl);
 
-// genero le mail
-for (let i = 0; i < 10; i++) {
+const mailGenerator = () => {
   fetch("https://flynn.boolean.careers/exercises/api/random/mail")
     .then((Response) => Response.json())
     .then((data) => {
       // stampo le mail nell'HTML
       mailContainerEl.innerHTML += `<li>${data.response}</li>`;
     });
+};
+
+// genero le mail
+for (let i = 0; i < 10; i++) {
+  mailGenerator();
 }
+
+// aggiungo mail attraverso un button
+buttonGeneraEl.addEventListener("click", (event) => {
+  mailGenerator();
+});
