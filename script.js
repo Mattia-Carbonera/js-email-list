0da7f588285327de88edec0c01d8a30403d35c0c
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-11-06 15:23:27 +0100

    fix error

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
+// // ottengo i nodi
+// const mailContainerEl = document.getElementById("mail-container");
+// const buttonGeneraEl = document.getElementById("button-genera");
+
+// const mailGenerator = () => {
+//   fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+//     .then((Response) => Response.json())
+//     .then((data) => {
+//       // stampo le mail nell'HTML
+//       mailContainerEl.innerHTML += `<li>${data.response}</li>`;
+//     });
+// };
+
+// // genero le mail
+// for (let i = 0; i < 10; i++) {
+//   mailGenerator();
+// }
+
+// // aggiungo mail attraverso un button
+// buttonGeneraEl.addEventListener("click", (event) => {
+//   mailGenerator();
+// });
+
 // ottengo i nodi
 const mailContainerEl = document.getElementById("mail-container");
 const buttonGeneraEl = document.getElementById("button-genera");
 
 const mailGenerator = () => {
-  fetch("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then((Response) => Response.json())
-    .then((data) => {
-      // stampo le mail nell'HTML
-      mailContainerEl.innerHTML += `<li>${data.response}</li>`;
-    });
+  mailContainerEl.innerHTML = ``;
+
+  for (let i = 0; i < 10; i++) {
+    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((Response) => Response.json())
+      .then((data) => {
+        // stampo le mail nell'HTML
+        mailContainerEl.innerHTML += `<li>${data.response}</li>`;
+      });
+  }
 };
 
 // genero le mail
-for (let i = 0; i < 10; i++) {
-  mailGenerator();
-}
+mailGenerator();
 
 // aggiungo mail attraverso un button
 buttonGeneraEl.addEventListener("click", (event) => {
